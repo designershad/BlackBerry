81b5d3ee8a763a1cc712a57fbb07d1cc077ed064
djoksimo <djoksimo@uwaterloo.ca>
2018-08-13 14:00:53 -0400

    nuked src folder and added sample Spark apps

--- Node/config.js
+++ Node/config.js
@@ -18,7 +18,7 @@
 module.exports = {
   // This domain is a string known to the BBM Enterprise server, which is
   // generally a GUID.
-  id_provider_domain: 'your_idp_domain',
+  id_provider_domain: '93a3cf1c-020e-471e-ab85-deeba8a42b00',
 
   // The environment of your BBM Enterprise server. Must be either 'Sandbox' or
   // 'Production'.
@@ -26,12 +26,12 @@ module.exports = {
 
   // Configuration information for firebase, to use for key storage.
   firebaseConfig: {
-    apiKey: "your_api_key",
-    authDomain: "your_auth_domain",
-    databaseURL: "your_database_url",
-    projectId: "your_project_id",
-    storageBucket: "your_storage_bucket",
-    messagingSenderId: "your_messaging_sender_id"
+    apiKey: "AIzaSyDOH3eLgNoEYDskmlwUpMRgkpweAf0IBnY",
+    authDomain: "sparkhackathon.firebaseapp.com",
+    databaseURL: "https://sparkhackathon.firebaseio.com",
+    projectId: "sparkhackathon",
+    storageBucket: "sparkhackathon.appspot.com",
+    messagingSenderId: "757672568611"
   },
 
   // Configuration information for a botlibre account. botlibre provides chatbot
@@ -44,17 +44,15 @@ module.exports = {
   // Configuration information for login to a google service account.
   googleConfig: {
     "type": "service_account",
-    "project_id": "your_project_id",
-    "private_key_id": "your_private_key_id",
-    "private_key": "your_private_key",
-    "client_email": "your_client_email",
-    "client_id": "your_client_id",
-    "auth_uri": "your_auth_uri",
-    "token_uri": "your_token_uri",
-    "auth_provider_x509_cert_url": "your_auth_provider_cert",
-    "client_x509_cert_url": "your_client_cert"
+    "project_id": "generated-media-191120",
+    "private_key_id": "286cd7965118e44c3febab3c78b671b57186634a",
+    "private_key":  "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC4XUWfm0JcztZH\ncriRR5VFGkRoQMK16+/6ynUtv6mgEF9NhyJYnx9RaXlFABX3dJQj8NPYT6WDQwvH\n7YAg4goSycDjo8MH6/97Pw/ciHojKeKPQbjJjU4QIvK2XFGyTNbEoXY+0a9L9YZO\nxbhMB01TI4+oAjJu6r0TluS7EgN6/DIkZIfjlymMirGg67tDb7qjPZHIkGiBI7H2\n02DWeIHnMhKCJe50+CDoMNnil4qSd4auOBL/tUI7rA4smooiyZSwAWjwp6k2Mhk+\nBzzrDUFSpw9ZK0rPEzojlCK2YA3HyQkEZG/78CYW4br6AnrS2hXIsQBH6NG/oAED\na9zcVInHAgMBAAECggEALlfz2m0Sv3WW05r43jwaP6acr4An8cb/KhGeZwOYxg2X\nh0uCzLgjDTnwK/Ibk3qQioBNIEDVICvIWFdRL/LiCVsBy7csnV8rd3WgrRT5bsFm\nnBAvPSryBohibojCWImMKx1TNfNLl1J+kmqNKz9ippV31QnIV61WHgh+MrASg+VW\nnI0kxrLQW4RxO0mEtFV20wmmldq7cpe0FKdL9blUVlbMRmyi2RU63R9uk8luSzUs\nWID8AOMiKO0jboCD+kjO+O6wSvsWAEfhy5eTAqXUqymNKlcpjxe+K+5Jg7eaBM8o\nExLGrfWxtGNxakawItLQwwefDi3wJhiyJ86Y39V3sQKBgQD/7Yxa8u4elzf2LdGx\n5iBgxOQrZMrDH6gKQ2+Ww3ni00u1dBNzT6JSgcCaDllk4nfgx3EZRbq5iVratTNT\nuczXtdP2akCuaHAdnFrMutlmIH60TJ8SDUu2MYRW3CZKixo+pMuE55w6ONX6wrc6\nokvxna+pvmyhQy0nOJB5UwAEvQKBgQC4apBsh94MMkHYM8Af1kyJMcopU58WZUG+\ncfpEA29YULxFyeaznwpYuoDZrwJ5R+QuUD1ZPC7i4z8zWQIqlmm2tAlwQuhg6LSO\nNabdnW7i61MVppJQlz4gF8jpwCnyiIduMFACmFDRkrEgcRmMb0odfQR5yLdMDYtu\nOB66HdpK0wKBgCfe+DjLE/HMGTnPFZYjhfPNeMaR40zWx7UhoGbaeUivJmJUfRs/\n0sgL0tetWJvw9EAeki4fwOFa1r6v8BkhHxqUBzuifgUt3Bmg3Dx895WDhnnMXhPP\n4nW17SojDgwa87HCtroz4xDm/d8dKrKFylMP/+fv9LiC4Kv1kfx0w9H5AoGAJ/MF\nyE0CgBffBavoQb1VpR7BEYTI4LwcqtncAiHQXAxZTLD5Jg4c2q3EPePYoFc/ifzd\n3VfZPFNIHKJ6gqlEy0xjAwurdVrTyj67ICT7oXKi8/y6k3TAA4nDf0eejqie1fLn\nUjiWwV+KuWFmzHJLVun/LzaY5oPEoaLMnTrcT/sCgYAflR582k7xGxVLj0sgPEC8\nHIJUguoltUv5uvCqi1MmLcQPaibmKiRuEVTx+QbVRuSnYuPRl8VwKiTAsBkxWkDi\n+lLIZ8Jgh2atrnke50yp/6RQoQdMY2G9JFzBoeowIWNdcu2rgTtUdT6L9NiTCcc7\n5uHOc4SrJBpmLfrX4WmZjw==\n-----END PRIVATE KEY-----\n",
+    "client_email": "djoksimovic@generated-media-191120.iam.gserviceaccount.com",
+    "client_id": "108160326438024734116",
+    "auth_uri": "https://www.googleapis.com/oauth2/v2/userinfo",
+    "token_uri": "https://www.googleapis.com/oauth2/v3/tokeninfo"
   },
 
   // A password to use to protect keys.
-  password: 'Protect my keys!'
+  password: 'BlackBerry123'
 };
